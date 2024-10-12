63400a633b5d5c0982499ccaaebf55e6bf851ab7
Amir Nourinia <amir.nouri.nia@gmail.com>
2024-10-12 20:04:38 +0200

    normal distribution with a test

--- include/pcg-random-lib.hpp
+++ include/pcg-random-lib.hpp
@@ -97,3 +97,52 @@ template <typename T>
     }
     return randomNumbers;
 }
+
+/**
+ *
+ * @param a lower bound included
+ * @param b upper bound included
+ * @return a random number in the range [a, b]
+ */
+template <typename T>
+[[nodiscard]] T GetRandomFloatFromNormal(T const mean, T const stddev)
+{
+    // Seed with a real random value, if available
+    pcg_extras::seed_seq_from<std::random_device> seed_source;
+
+    // Make a random number engine
+    pcg64 rng(seed_source);
+
+    // values near the mean are the most likely
+    // standard deviation affects the dispersion of generated values from the mean
+    std::lognormal_distribution<T> normal_distribution{mean, stddev};
+    T const randomNumber = normal_distribution(rng);
+    return randomNumber;
+}
+
+/**
+ *
+ * @param mean lower bound included
+ * @param stddev upper bound included
+ * @param count number of integers returned
+ * @return numbers in the range [a, b]
+ */
+template <typename T>
+[[nodiscard]] inline Vector<T> GetRandomFloatsFromNormal(T const mean, T const stddev, Int64 const count)
+{
+    // Seed with a real random value, if available
+    pcg_extras::seed_seq_from<std::random_device> seed_source;
+
+    // Make a random number engine
+    pcg64 rng(seed_source);
+
+    std::lognormal_distribution<T> normal_distribution(mean, stddev);
+
+    Vector<T> randomNumbers(count);
+
+    for (Int64 i = 0; i < count; ++i)
+    {
+        randomNumbers.at(i) = normal_distribution(rng);
+    }
+    return randomNumbers;
+}
--- src/pcg-random-lib.cpp
+++ src/pcg-random-lib.cpp
@@ -5,7 +5,13 @@ int main()
 
     {
         auto const t1 = std::chrono::steady_clock::now();
-
+        [[maybe_unused]] auto const randomNumbers = GetRandomFloatsFromNormal<Float128>(0, 1, 100'000);
+        auto const t2 = std::chrono::steady_clock::now();
+        auto const duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+        std::cout << "\n" << "Duration for float number generation: " << duration << "us" << "\n";
+    }
+    {
+        auto const t1 = std::chrono::steady_clock::now();
         for (int i = 0; i < 100'000; ++i)
         {
             [[maybe_unused]] const auto randomNumber = GetRandomFloatFromUnifrom(0.0, 1000.0);
@@ -16,7 +22,7 @@ int main()
 
         auto const t2 = std::chrono::steady_clock::now();
         auto const duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
-        std::cout << "Duration for float number generation: " << duration << "us" << "\t";
+        std::cout << "Duration for float number generation: " << duration << "us" << "\n";
     }
 
     {
@@ -32,7 +38,7 @@ int main()
 
         auto const t2 = std::chrono::steady_clock::now();
         auto const duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
-        std::cout << "Duration for int number generation: " << duration << "us" << "\t";
+        std::cout << "Duration for int number generation: " << duration << "us" << "\n";
     }
 
     std::cout << std::endl;
